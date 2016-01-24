364d019986286b93daf0fd5e21facd0d9d702710
Parminder Singh <email": null>
2016-01-24 16:11:45 -0600

    fix broken funtionality

---
+++ app.js
@@ -0,0 +1,67 @@
+angular.module('worldEvents', [])
+    .factory('posts', [function() {
+        var p = {
+            posts: []
+
+        };
+        return p;
+    }])
+    .controller('MainCtrl', ['$scope', 'posts', function($scope, posts) {
+
+        $scope.posts = posts.posts;
+
+        $scope.addPost = function() {
+            if (!$scope.title || $scope.title === '') {
+                return;
+            } //post cant be sumbitted if blank
+            $scope.posts.push({
+                title: $scope.title,
+                link: $scope.link,
+                upvotes: 0
+            });
+
+            $scope.title = '';
+            $scope.link = '';
+        };
+
+        $scope.incrementUpvotes = function(post) {
+            post.upvotes += 1;
+        };
+
+        $scope.incrementDownvotes = function(post) {
+            post.upvotes -= 1;
+        };
+
+
+    }]);
+
+    // .controller('PostsCtrl', [
+    //   '$scope',
+    //   '$stateParams',
+    //   'posts',
+    //   function($scope,$stateParams, posts){
+
+
+    //   $scope.post = posts.posts[$stateParams.id];
+
+    //   }]);
+
+
+// angular.module('worldEvents', ['ui.router'])
+//     .config([
+//         '$stateProvider',
+//         '$urlRouterProvder',
+//         function($stateProvider, $urlRouterProvider) {
+//             $stateProvider
+//                 .state('home', {
+//                     url: '/home',
+//                     templateUrl: '/home.html',
+//                     controller: 'MainCtrl'
+//                 });
+//                 .state('posts, {
+//                   url: '/posts/{id}',
+//                   templateUrl: '/posts.html',
+//                   controller: 'PostsCtrl'});
+//             $urlRouterProvider.otherwise('home');
+//         }
+//     ])
