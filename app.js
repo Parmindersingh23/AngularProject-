f7fc5c6a090ce12b1b4ed67f5b9805a02e6c84c7
Parminder Singh <email": null>
2016-01-24 16:16:36 -0600

    fix merge issue

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 angular.module('worldEvents', [])
-<<<<<<< HEAD
     .factory('posts', [function() {
         var p = {
             posts: []
@@ -66,48 +65,5 @@ angular.module('worldEvents', [])
 //             $urlRouterProvider.otherwise('home');
 //         }
 //     ])
-=======
-  .factory('posts', [function(){
-    var p = {
-      posts: []
 
-    };
-    return p;
-  }])
-  .controller('MainCtrl', ['$scope', 'posts', function($scope, posts){
 
-  $scope.posts = posts.posts;
-
-   $scope.addPost = function(){
-    if(!$scope.title || $scope.title === '')
-      { return; } //post cant be sumbitted if blank
-      $scope.posts.push({
-        title: $scope.title,
-        link: $scope.link,
-        upvotes: 0
-      });
-
-      $scope.title ='';
-      $scope.link='';
-    };
-
-   $scope.incrementUpvotes = function(post){
-      post.upvotes +=1;
-   };
-
-}]);
-
-angular.module('worldEvents', ['ui.router'])
-  .config([
-    '$stateProvider',
-    'urlRouterProvder',
-    function($stateProvider, $urlRouterProvder){
-      $stateProvider
-        .state('home',{
-          url: '/home',
-          templateUrl: '/home.html',
-          controller: 'MainCtrl'
-        });
-      $urlRouterProvder.otherwise('home');
-    }])
->>>>>>> 7ad1b862bc752360493bf1fb89b224e6d48bb6ec
